3348edd92d0604525ddb98413e39be3fc64a18f7
Reggie Williams <rlwjr433@gmail.com>
2023-06-22 10:12:29 -0500

    practicing commenting (algorithm, in-line, & multi-line

--- hullo_wurld_pythonv2.py
+++ hullo_wurld_pythonv2.py
@@ -1,7 +1,20 @@
+#I could write an algorithm up here in comments. sucess.  next learn how to color code. is there a python specific way to do it? 
+#
+
 amiarealboy = True
 
 if amiarealboy:
     print("I'm seriously Pinnochio, and it's true, I'm a real boy!")
     amiarealboy = False
     print(False)
+
+
+
+#practicing comments with color coding (failed)
+#practicing comments with color coding
+#practicing comments with color coding
+#practicing comments with color coding
+#practicing comments with color coding
+#practicing comments with color coding
+
  
